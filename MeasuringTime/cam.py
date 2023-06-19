862a7fed298fe1f3191cd7ccd18a09b5df35041a
Matthew Kotula <mkotula4@gmail.com>
2023-06-19 20:11:35 +0200

    FinishedCamera(19.06.2023)

--- MeasuringTime/cam.py
+++ MeasuringTime/cam.py
@@ -15,54 +15,86 @@ How to use camera?
 
 import cv2
 import os
+import time
 
-# Camera
-cam = cv2.VideoCapture(0)
 
-pngName = "../background.png"
+def captureBackground(pngName, cam):
+    ret, frame = cam.read()
+    cv2.imwrite(pngName, frame)
 
-if os.path.isfile(pngName):
+
+def formatFrame(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.GaussianBlur(frame, (21, 21), 0)
+    return frame
+
+
+def readTime(pngName, cam, sensitivity):
+    startTime = None
+    finishTime = None
+    runTime = None
+
+    background = cv2.imread(pngName)
+    background = formatFrame(background)
+
+    while True:
+        _, frame = cam.read()
+
+        frame = formatFrame(frame)
+
+        diff = cv2.absdiff(background, frame)
+        thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        resized = cv2.resize(thresh, (25, 25), fx=0, fy=0, interpolation=cv2.THRESH_BINARY)
+
+        cv2.imshow('resized', resized)
+
+        height, width = resized.shape
+
+        countWhite = 0
+        for x in range(0, width):
+            for y in range(0, height):
+                if resized[x, y] > 0:
+                    countWhite += 1
+
+        if countWhite > (height * width) * (sensitivity / 100):
+            if startTime is None:
+                startTime = time.time()
+                print("-=- Started measuring time -=-")
+
+            finishTime = time.time()
+            tempTime = finishTime - startTime
+
+            if tempTime > 3:
+                if runTime is None:
+                    runTime = tempTime
+                    print("-=- Finished measuring time -=-")
+
+        if cv2.waitKey(1) == ord('q'):
+            if runTime is None:
+                runTime = 0
+
+            return runTime
+
+
+def startCam():
+    cam = cv2.VideoCapture(0)
+    pngName = "../background.png"
+    sensitivity = 30
+
+    if os.path.isfile(pngName):
+        os.remove(pngName)
+
+    captureBackground(pngName, cam)
+    runTime = readTime(pngName, cam, sensitivity)
+
     os.remove(pngName)
+    cam.release()
+    cv2.destroyAllWindows()
 
-ret, frame = cam.read()
-cv2.imwrite(pngName, frame)
+    runTime = round(runTime, 3)
+    return runTime
 
-background = cv2.imread(pngName)
-background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
-background = cv2.GaussianBlur(background, (21, 21), 0)
 
-while True:
-    _, frame = cam.read()
-
-    g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    g = cv2.GaussianBlur(g, (21, 21), 0)
-
-    diff = cv2.absdiff(background, g)
-
-    thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.dilate(thresh, None, iterations=2)
-
-    resized = cv2.resize(thresh, (10, 10), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
-
-    cv2.imshow('resized', resized)
-
-    height, width = resized.shape
-
-    countWhite = 0
-    for x in range(0, width):
-        for y in range(0, height):
-            if resized[x, y] == 255:
-                countWhite += 1
-
-    if countWhite >= 30:
-        print(1)
-    else:
-        print(0)
-
-    if cv2.waitKey(1) == ord('x'):
-        break
-
-os.remove(pngName)
-cam.release()
-cv2.destroyAllWindows()
-os.remove(pngName)
+print(startCam())
