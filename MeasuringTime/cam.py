07ca197ea9eb8df72310b4c7fbafd06550061d6e
Matthew Kotula <mkotula4@gmail.com>
2023-06-19 20:51:18 +0200

    Finished&DocumentedCamera(19.06.2023)

--- MeasuringTime/cam.py
+++ MeasuringTime/cam.py
@@ -1,16 +1,5 @@
 """
-Install OpenCV
-1. Ctrl + Alt + S
-2. Go to project -> Python Interpreter -> Click '+' icon (top left)
-3. Type 'opencv' and install
-
-How to use camera?
-1. Run 'takeSample.py'
-2. Position your camera, so it can only see the background
-3. Press 'c' on the keyboard
-4. Program will end and give you file named 'background.png',
-    it is vital for noice detection
-5. Run this script
+Class takes care of measuring runners time.
 """
 
 import cv2
@@ -18,83 +7,99 @@ import os
 import time
 
 
-def captureBackground(pngName, cam):
-    ret, frame = cam.read()
-    cv2.imwrite(pngName, frame)
+class Camera:
+    def __init__(self, sensitivity):
+        self.pngName = "../background.png"
+        self.sensitivity = sensitivity
+        self.camera = cv2.VideoCapture(0)
 
+    def captureBackground(self):
+        """
+        Method simply takes a frame from camera and save it as .png file
+        """
+        ret, frame = self.camera.read()
+        cv2.imwrite(self.pngName, frame)
 
-def formatFrame(frame):
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.GaussianBlur(frame, (21, 21), 0)
-    return frame
+    @staticmethod
+    def formatFrame(frame):
+        """
+        Method format given frame to the shades of gray
 
+        :param frame: Frame we want to format
+        :return: Formatted frame
+        """
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.GaussianBlur(frame, (21, 21), 0)
+        return frame
 
-def readTime(pngName, cam, sensitivity):
-    startTime = None
-    finishTime = None
-    runTime = None
+    def readTime(self):
+        """
+        Core function of this class. Method takes a frame compares it with background. If movement is detected method
+        measure time and returns it when user preses 'q' to quit.
+        :return: Measured time from first detected movement
+            to the second detected movement min. 3 seconds after the first one.
+        """
+        startTime = None
+        runTime = None
 
-    background = cv2.imread(pngName)
-    background = formatFrame(background)
+        background = cv2.imread(self.pngName)
+        background = self.formatFrame(background)
 
-    while True:
-        _, frame = cam.read()
+        while True:
+            _, frame = self.camera.read()
 
-        frame = formatFrame(frame)
+            frame = self.formatFrame(frame)
 
-        diff = cv2.absdiff(background, frame)
-        thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.dilate(thresh, None, iterations=2)
+            diff = cv2.absdiff(background, frame)
+            thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
 
-        resized = cv2.resize(thresh, (25, 25), fx=0, fy=0, interpolation=cv2.THRESH_BINARY)
+            resized = cv2.resize(thresh, (25, 25), fx=0, fy=0, interpolation=cv2.THRESH_BINARY)
 
-        cv2.imshow('resized', resized)
+            cv2.imshow('resized', resized)
 
-        height, width = resized.shape
+            height, width = resized.shape
 
-        countWhite = 0
-        for x in range(0, width):
-            for y in range(0, height):
-                if resized[x, y] > 0:
-                    countWhite += 1
+            countWhite = 0
+            for x in range(0, width):
+                for y in range(0, height):
+                    if resized[x, y] > 0:
+                        countWhite += 1
 
-        if countWhite > (height * width) * (sensitivity / 100):
-            if startTime is None:
-                startTime = time.time()
-                print("-=- Started measuring time -=-")
+            if countWhite > (height * width) * (self.sensitivity / 100):
+                if startTime is None:
+                    startTime = time.time()
+                    print("-=- Started measuring time -=-")
 
-            finishTime = time.time()
-            tempTime = finishTime - startTime
+                finishTime = time.time()
+                tempTime = finishTime - startTime
 
-            if tempTime > 3:
+                if tempTime > 3:
+                    if runTime is None:
+                        runTime = tempTime
+                        print("-=- Finished measuring time -=-")
+
+            if cv2.waitKey(1) == ord('q'):
                 if runTime is None:
-                    runTime = tempTime
-                    print("-=- Finished measuring time -=-")
+                    runTime = 0
 
-        if cv2.waitKey(1) == ord('q'):
-            if runTime is None:
-                runTime = 0
+                return runTime
 
-            return runTime
+    def start(self):
+        """
+        Method that starts process of time measurement.
 
+        :return: Measured time
+        """
+        if os.path.isfile(self.pngName):
+            os.remove(self.pngName)
 
-def startCam():
-    cam = cv2.VideoCapture(0)
-    pngName = "../background.png"
-    sensitivity = 30
+        self.captureBackground()
+        runTime = self.readTime()
 
-    if os.path.isfile(pngName):
-        os.remove(pngName)
+        os.remove(self.pngName)
+        self.camera.release()
+        cv2.destroyAllWindows()
 
-    captureBackground(pngName, cam)
-    runTime = readTime(pngName, cam, sensitivity)
-
-    os.remove(pngName)
-    cam.release()
-    cv2.destroyAllWindows()
-
-    runTime = round(runTime, 3)
-    return runTime
-
-
-print(startCam())
+        runTime = round(runTime, 3)
+        return runTime
