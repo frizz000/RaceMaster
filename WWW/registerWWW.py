48c61bf041591c4326bb176fcd9c11ca81c27aec
Piotr Jalocha <piotr.jalocha2001@gmail.com>
2023-06-21 13:21:52 +0200

    dokumetacja (integrated)(20.06)

--- WWW/registerWWW.py
+++ WWW/registerWWW.py
@@ -1,41 +1,44 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+class RegisterWWW:
+    def __init__(self):
+        self.app = Flask(__name__, template_folder='../templates', static_folder='../static')
+        self.setup_routes()
 
-@app.route('/', methods=['GET', 'POST'])
-def register():
-    """
-    Metod to register new player to database
-    :param request:
-    :return:
-    """
-    conn = sqlite3.connect('../zawody.db')
-    c = conn.cursor()
+    def setup_routes(self):
+        @self.app.route('/', methods=['GET', 'POST'])
+        def register():
+            conn = sqlite3.connect('../zawody.db')
+            c = conn.cursor()
 
-    if request.method == 'POST':
-        imie = request.form['imie']
-        nazwisko = request.form['nazwisko']
-        dataUrodzenia = request.form['dataUrodzenia']
-        nrTelefonu = request.form['nrTelefonu']
-        daneOpiekuna = request.form['daneOpiekuna']
-        pojazd = request.form['pojazd']
-        Kategoria_nazwa = request.form['Kategoria']
+            if request.method == 'POST':
+                imie = request.form['imie']
+                nazwisko = request.form['nazwisko']
+                dataUrodzenia = request.form['dataUrodzenia']
+                nrTelefonu = request.form['nrTelefonu']
+                daneOpiekuna = request.form['daneOpiekuna']
+                pojazd = request.form['pojazd']
+                Kategoria_nazwa = request.form['Kategoria']
 
-        c.execute("SELECT id FROM Kategoria WHERE nazwa=?", (Kategoria_nazwa,))
-        Kategoria_id = c.fetchone()[0]
+                c.execute("SELECT id FROM Kategoria WHERE nazwa=?", (Kategoria_nazwa,))
+                Kategoria_id = c.fetchone()[0]
 
-        c.execute('''
-            INSERT INTO Zawodnik (imie, nazwisko, dataUrodzenia, nrTelefonu, daneOpiekuna, pojazd, Kategoria_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (imie, nazwisko, dataUrodzenia, nrTelefonu, daneOpiekuna, pojazd, Kategoria_id))
-        conn.commit()
+                c.execute('''
+                    INSERT INTO Zawodnik (imie, nazwisko, dataUrodzenia, nrTelefonu, daneOpiekuna, pojazd, Kategoria_id)
+                    VALUES (?, ?, ?, ?, ?, ?, ?)
+                ''', (imie, nazwisko, dataUrodzenia, nrTelefonu, daneOpiekuna, pojazd, Kategoria_id))
+                conn.commit()
 
-        return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    else:
-        c.execute("SELECT nazwa FROM Kategoria")
-        kategorie = [row[0] for row in c.fetchall()]
-        return render_template('register.html', kategorie=kategorie)
+                return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+            else:
+                c.execute("SELECT nazwa FROM Kategoria")
+                kategorie = [row[0] for row in c.fetchall()]
+                return render_template('register.html', kategorie=kategorie)
+
+    def run(self):
+        self.app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    my_app = RegisterWWW()
+    my_app.run()
--- WWW/tableWWW.py
+++ WWW/tableWWW.py
@@ -1,33 +1,35 @@
 from flask import Flask, render_template
 import sqlite3
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+class TableWWW:
+    def __init__(self):
+        self.app = Flask(__name__, template_folder='../templates', static_folder='../static')
+        self.setup_routes()
 
+    def setup_routes(self):
+        @self.app.route('/')
+        def home():
+            conn = sqlite3.connect('../zawody.db')
+            c = conn.cursor()
 
-@app.route('/')
-def home():
-    """
-    Method to show table with all players
-    :return:
-    """
-    conn = sqlite3.connect('../zawody.db')
-    c = conn.cursor()
+            c.execute('''
+                    SELECT Zawodnik.id, imie, nazwisko, dataUrodzenia, Kategoria.nazwa, GROUP_CONCAT(Przejazd.czasPrzejazdu)
+                    FROM Zawodnik
+                    JOIN Kategoria ON Zawodnik.Kategoria_id = Kategoria.id
+                    LEFT JOIN Przejazd ON Zawodnik.id = Przejazd.Zawodnik_id
+                    GROUP BY Zawodnik.id
+                ''')
+            zawodnicy = c.fetchall()
 
-    c.execute('''
-            SELECT Zawodnik.id, imie, nazwisko, dataUrodzenia, Kategoria.nazwa, GROUP_CONCAT(Przejazd.czasPrzejazdu)
-            FROM Zawodnik
-            JOIN Kategoria ON Zawodnik.Kategoria_id = Kategoria.id
-            LEFT JOIN Przejazd ON Zawodnik.id = Przejazd.Zawodnik_id
-            GROUP BY Zawodnik.id
-        ''')
-    zawodnicy = c.fetchall()
+            zawodnicy = [(id, imie, nazwisko, dataUrodzenia, nazwa, "brak czasu" if czasy is None else czasy) for id, imie, nazwisko, dataUrodzenia, nazwa, czasy in zawodnicy]
 
-    zawodnicy = [(id, imie, nazwisko, dataUrodzenia, nazwa, "brak czasu" if czasy is None else czasy) for id, imie, nazwisko, dataUrodzenia, nazwa, czasy in zawodnicy]
+            max_przejazdy = max(len(zawodnik[5].split(',')) for zawodnik in zawodnicy)
 
-    max_przejazdy = max(len(zawodnik[5].split(',')) for zawodnik in zawodnicy)
+            return render_template('index.html', zawodnicy=zawodnicy, max_przejazdy=max_przejazdy)
 
-    return render_template('index.html', zawodnicy=zawodnicy, max_przejazdy=max_przejazdy)
-
+    def run(self):
+        self.app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    my_app = TableWWW()
+    my_app.run()
